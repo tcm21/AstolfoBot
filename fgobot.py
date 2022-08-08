0f0b7e470333456fa5f5ea05fc26e93e14e12941
TCM <tuancm91@gmail.com>
2022-08-09 00:06:06 +0900

    add search skills/NP by buffs

--- fgobot.py
+++ fgobot.py
@@ -2,6 +2,7 @@ import configparser
 import requests
 import json
 import interactions
+from interactions import autodefer
 
 configparser = configparser.ConfigParser()
 configparser.read('env.config')
@@ -94,19 +95,20 @@ def get_skills_with_type(type: str, flag: str = "skill", target: str = ""):
     Returns:
         A list of skill objects with the specified effect.
     """
+    if type == "": return None
     functions = get_functions(type, target)
     found_skills = get_skills_from_functions(functions, flag)
     return found_skills
 
 def get_skills_with_buff(buffType: str = "", flag: str = "skill"):
-    if buffType == "": return []
-    url = f"https://api.atlasacademy.io/basic/JP/buff/search?reverse=true&reverseDepth=skillNp&reverseData=basic&type={buffType}"
+    if buffType == "": return None
+    url = f"https://api.atlasacademy.io/basic/JP/buff/search?reverse=true&reverseDepth=servant&reverseData=basic&type={buffType}"
     response = requests.get(url)
     buffs = json.loads(response.text)
     skills = []
     for buff in buffs:
         functions = buff.get("reverse").get("basic").get("function")
-        skills = get_skills_from_functions(functions, flag)
+        skills.extend(get_skills_from_functions(functions, flag))
 
     return skills
 
@@ -140,21 +142,27 @@ def get_skills(type: str, type2: str = "", flag: str = "skill", target: str = ""
     embed.add_field("Type 1", fn_names_json.get(type), True)
     if type2 != "": embed.add_field("Type 2", fn_names_json.get(type2), True)
     if target != "": embed.add_field("Target", tg_names_json.get(target), True)
+    if buffType1 != "": embed.add_field("Buff 1", buff_names_json.get(buffType1), True)
+    if buffType2 != "": embed.add_field("Buff 2", buff_names_json.get(buffType2), True)
 
+    maxLimit = 15
+    count = 0
     for skill in matched_skills_list:
         result_str.append(f"・{skill.get('name')}\n")
         servants = skill.get('reverse').get('basic').get('servant')
         servantList = []
         for servant in servants:
+            if count == maxLimit: break
             if servant.get('name') == "" or servant.get('type') == "servantEquip":
                 continue
             servant_str = f"{servant.get('name')} {servant.get('className')}\n"
             if servant_str not in servantList:
                 servantList.append(servant_str)
                 embed.add_field(servant_str, f"[{skill.get('name')}](https://apps.atlasacademy.io/db/JP/{'skill' if flag == 'skill' else 'noble-phantasm'}/{skill.get('id')})")
+                count+=1
                 result_str.append(servant_str)
 
-    if len(result_str) > 0:
+    if count > 0:
         return embed
     else:
         embed.add_field("Not found.", "Try different queries")
@@ -163,14 +171,15 @@ def get_skills(type: str, type2: str = "", flag: str = "skill", target: str = ""
 def common_elements(*lists):
     common_list = []
     for list in lists:
-        if len(list) == 0:
-            continue
+        if list == None: continue
+        if len(list) == 0: return []
         if len(common_list) == 0 and len(list) > 0:
-            common_list = list
+            common_list.extend(list)
             continue
-        common_list = [element for element in list if element in common_list]
+        common_list = [element for element in common_list if element in list]
     return common_list
 
+# Commands
 @bot.command(
     scope=[760776452609802250],
 )
@@ -184,11 +193,49 @@ async def servant(ctx: interactions.CommandContext, name: str):
 @interactions.option(str, name="type", description="Effect 1", required=True, autocomplete=True)
 @interactions.option(str, name="type2", description="Effect 2", required=False, autocomplete=True)
 @interactions.option(str, name="target", description="Target", required=False, autocomplete=True)
-async def skill(ctx: interactions.CommandContext, type: str, type2: str = "", target: str = ""):
-    await ctx.send(embeds=get_skills(type, type2, "skill", target))
+@interactions.option(str, name="buff", description="Buff 1", required=False, autocomplete=True)
+@interactions.option(str, name="buff2", description="Buff 2", required=False, autocomplete=True)
+async def skill(
+    ctx: interactions.CommandContext,
+    type: str,
+    type2: str = "",
+    target: str = "",
+    buff: str = "",
+    buff2: str = "",
+    ):
+    await ctx.defer()
+    embed = get_skills(type, type2, "skill", target, buff, buff2)
+    await ctx.send(embeds=embed)
 
+@bot.command(
+    scope=[760776452609802250],
+)
+@interactions.option(str, name="type", description="Effect 1", required=True, autocomplete=True)
+@interactions.option(str, name="type2", description="Effect 2", required=False, autocomplete=True)
+@interactions.option(str, name="target", description="Target", required=False, autocomplete=True)
+@interactions.option(str, name="buff", description="Buff 1", required=False, autocomplete=True)
+@interactions.option(str, name="buff2", description="Buff 2", required=False, autocomplete=True)
+async def np(
+    ctx: interactions.CommandContext,
+    type: str,
+    type2: str = "",
+    target: str = "",
+    buff: str = "",
+    buff2: str = "",
+    ):
+    await ctx.defer()
+    embed = get_skills(type, type2, "NP", target, buff, buff2)
+    await ctx.send(embeds=embed)
+
+# Autocomplete functions
 with open('function_names.json') as fn_names:
     fn_names_json = json.load(fn_names)
+
+with open('target_names.json') as tg_names:
+    tg_names_json = json.load(tg_names)
+
+with open('buff_names.json') as buff_names:
+    buff_names_json = json.load(buff_names)
 
 def populateSkillNamesList(input_value: str):
     options = fn_names_json.keys()
@@ -202,9 +249,6 @@ def populateSkillNamesList(input_value: str):
         choices.append(interactions.Choice(name=text, value=option))
     return choices
 
-with open('target_names.json') as tg_names:
-    tg_names_json = json.load(tg_names)
-
 def populateTargetList(input_value: str):
     options = tg_names_json.keys()
     filteredOptions = [
@@ -217,37 +261,41 @@ def populateTargetList(input_value: str):
         choices.append(interactions.Choice(name=text, value=option))
     return choices
 
+def populateBuffList(input_value: str):
+    options = buff_names_json.keys()
+    filteredOptions = [
+        option for option in options
+        if (input_value.upper() in option.upper() or input_value.upper() in buff_names_json.get(option, option).upper())
+    ]
+    choices = []
+    for option in filteredOptions[0:24]:
+        text = buff_names_json.get(option, option)
+        choices.append(interactions.Choice(name=text, value=option))
+    return choices
+
 @bot.autocomplete(command="skill", name="type")
-async def autocomplete_choice_list(ctx: interactions.CommandContext, type: str = ""):
-    await ctx.populate(populateSkillNamesList(type))
-
-@bot.autocomplete(command="skill", name="type2")
-async def autocomplete_choice_list(ctx: interactions.CommandContext, type2: str = ""):
-    await ctx.populate(populateSkillNamesList(type2))
-
-@bot.autocomplete(command="skill", name="target")
-async def autocomplete_choice_list(ctx: interactions.CommandContext, target: str = ""):
-    await ctx.populate(populateTargetList(target))
-
-@bot.command(
-    scope=[760776452609802250],
-)
-@interactions.option(str, name="type", description="Effect 1", required=True, autocomplete=True)
-@interactions.option(str, name="type2", description="Effect 2", required=False, autocomplete=True)
-@interactions.option(str, name="target", description="Target", required=False, autocomplete=True)
-async def np(ctx: interactions.CommandContext, type: str, type2: str = "", target: str = ""):
-    await ctx.send(embeds=get_skills(type, type2, "NP", target))
-
 @bot.autocomplete(command="np", name="type")
 async def autocomplete_choice_list(ctx: interactions.CommandContext, type: str = ""):
     await ctx.populate(populateSkillNamesList(type))
 
+@bot.autocomplete(command="skill", name="type2")
 @bot.autocomplete(command="np", name="type2")
 async def autocomplete_choice_list(ctx: interactions.CommandContext, type2: str = ""):
     await ctx.populate(populateSkillNamesList(type2))
 
+@bot.autocomplete(command="skill", name="target")
 @bot.autocomplete(command="np", name="target")
 async def autocomplete_choice_list(ctx: interactions.CommandContext, target: str = ""):
     await ctx.populate(populateTargetList(target))
+    
+@bot.autocomplete(command="skill", name="buff")
+@bot.autocomplete(command="np", name="buff")
+async def autocomplete_choice_list(ctx: interactions.CommandContext, buff: str = ""):
+    await ctx.populate(populateBuffList(buff))
+
+@bot.autocomplete(command="skill", name="buff2")
+@bot.autocomplete(command="np", name="buff2")
+async def autocomplete_choice_list(ctx: interactions.CommandContext, buff2: str = ""):
+    await ctx.populate(populateBuffList(buff2))
 
 bot.start()
